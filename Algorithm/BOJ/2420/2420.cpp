a36d4c6c842e34f0d781d466fd9c57e16007cf2b
SEWO <Se93Wo@gmail.com>
2018-08-06 14:51:54 +0900

    SEWO

---
+++ Algorithm/BOJ/2420/2420.cpp
@@ -0,0 +1,13 @@
+﻿#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	long N, M;
+	long long ans;
+	scanf("%ld %ld", &N, &M);
+
+	ans = abs(N - M);
+
+	printf("%lld", ans);
+}
