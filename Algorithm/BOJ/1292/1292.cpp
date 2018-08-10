3d2aec57a433df966f54ff2abcd8fc63a370633f
SEWO <Se93Wo@gmail.com>
2018-08-11 01:15:58 +0900

    SEWO

---
+++ Algorithm/BOJ/1292/1292.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+	int cnt = 0, ans = 0;
+	cin >> A >> B;
+
+	for (int i = 1; i <= B;) {
+		cnt++;
+		for (int j = 1; j <= cnt; j++) {
+			if (A <= i && i <= B)
+				ans += cnt;
+			i++;
+		}
+	}
+
+	cout << ans;
+}
---
+++ Algorithm/BOJ/2693/2693.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	while (T--) {
+		int A[10] = { 0, };
+
+		for (int i = 0; i < 10; i++) {
+			cin >> A[i];
+		}
+
+		sort(A, A + 10);
+		
+		cout << A[7] << endl;
+	}
+}
