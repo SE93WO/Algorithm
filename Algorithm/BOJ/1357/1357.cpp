365e962998518582676979aacc29e654deb09c4d
SEWO <Se93Wo@gmail.com>
2018-08-19 14:53:09 +0900

    SEWO

---
+++ Algorithm/BOJ/1357/1357.cpp
@@ -0,0 +1,13 @@
+﻿#include<cstdio>
+
+int a, b;
+int f(int x) {
+	int r = 0;
+	for (; x; x /= 10) r = r * 10 + x % 10;
+	return r;
+}
+int main() {
+	scanf("%d %d", &a, &b);
+	printf("%d", f(f(a) + f(b)));
+	return 0;
+}
---
+++ Algorithm/BOJ/1735/1735.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int EU(int x, int y) {
+	int n;
+	while (true) {
+		n = x % y;
+		if (n == 0) return y;
+		x = y;
+		y = n;
+	}
+}
+
+int main() {
+	int numerator, denominator;
+	int A, B, C, D;
+	int gcm;
+
+	cin >> A >> B;
+	cin >> C >> D;
+	numerator = A * D + B * C;
+	denominator = B * D;
+
+	gcm = EU(numerator, denominator);
+	cout << numerator / gcm << " " << denominator / gcm;
+}
