8c425ffe9eea7a5effa3a5d6c8a2e9aec9cb5b5c
SEWO <Se93Wo@gmail.com>
2018-08-31 07:07:23 +0900

    SEWO

---
+++ Algorithm/BOJ/2774/2774.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int T;
+	string input;
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int num[10] = { 0, };
+		int cnt = 0;
+		cin >> input;
+		for (int j = 0; j < input.length(); j++) {
+			int temp = input[j] - '0';
+			num[temp]++;
+		}
+
+		for (int k = 0; k < 10; k++) {
+			if (num[k] > 0)
+				cnt++;
+		}
+
+		cout << cnt << endl;
+	}
+
+ }
