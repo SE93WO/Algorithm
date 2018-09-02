f0cbf15da25c931ed89e6667cc26d61aaee17ec9
SEWO <Se93Wo@gmail.com>
2018-09-02 10:10:56 +0900

    SEWO

---
+++ Algorithm/BOJ/2133/2133.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main() {
+	int N;
+	int dp[31] = { 1, 0, 3, };
+	cin >> N;
+
+	for (int i = 3; i <= N; i++) {
+		dp[i] += dp[i - 2] * 3;
+
+		for (int j = 4; j <= i; j += 2)
+			dp[i] += dp[i - j] * 2;
+	}
+
+	cout << dp[N];
+}
