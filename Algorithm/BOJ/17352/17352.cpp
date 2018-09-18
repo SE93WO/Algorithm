2254c4892c0a32a8a57f6030ea2fdf1c4cbd80e7
SEWO <Se93Wo@gmail.com>
2018-09-18 15:17:39 +0900

    SEWO

---
+++ Algorithm/BOJ/17352/17352.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int k, input;
+	cin >> k;
+	while (k--) {
+		cin >> input;
+		for (int i = 0; i < input; i++) {
+			cout << "=";
+		}
+		cout << endl;
+	}
+}
