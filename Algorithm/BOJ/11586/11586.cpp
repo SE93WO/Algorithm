3349c61f4e1a20c3e91862b37f418c410cc0f0a6
SEWO <Se93Wo@gmail.com>
2018-09-25 19:15:08 +0900

    SEWO

---
+++ Algorithm/BOJ/11586/11586.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N, K;
+	string input[101];
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> input[i];
+	}
+	cin >> K;
+
+	if (K == 1) {
+		for (int i = 0; i < N; i++) {
+			cout << input[i] << endl;
+		}
+	}
+	else if (K == 2) {
+		for (int i = 0; i < N; i++) {
+			for (int j = N - 1; j >= 0; j--) {
+				cout << input[i][j];
+			}
+			cout << endl;
+		}
+	}
+	else if (K == 3) {
+		for (int i = N - 1; i >= 0; i--) {
+			cout << input[i] << endl;
+		}
+	}
+}
