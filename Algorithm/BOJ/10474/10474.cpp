f44add1e012078434c02c9a5aa083f557944955f
SEWO <Se93Wo@gmail.com>
2018-09-27 18:21:31 +0900

    SEWO

---
+++ Algorithm/BOJ/10474/10474.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int denominator, numerator, cnt;
+
+	while (true) {
+		cnt = 0;
+		cin >> numerator >> denominator;
+
+		if (numerator + denominator == 0)
+			break;
+
+		while (true) {
+			if (numerator >= denominator) {
+				numerator -= denominator;
+					cnt++;
+			}
+			else
+				break;
+		}
+
+		cout << cnt << " " << numerator << " / " << denominator << endl;
+	}
+}
---
+++ Algorithm/BOJ/5656/5656.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void print(bool ans, int n) {
+	if (ans) {
+		cout << "Case " << n << ": " << "true" << endl;
+	} else
+		cout << "Case " << n << ": " << "false" << endl;
+}
+
+int main() {
+	int a, b, n = 1;
+	string op;
+
+	while (true) {
+		bool ans;
+		cin >> a >> op >> b;
+		if (op == "E") {
+			break;
+		}
+
+		if (op == ">") {
+			ans = a > b ? true : false;
+			print(ans, n);
+			n++;
+		}
+		else if (op == ">=") {
+			ans = a >= b ? true : false;
+			print(ans, n);
+			n++;
+		}
+		else if (op == "<") {
+			ans = a < b ? true : false;
+			print(ans, n);
+			n++;
+		}
+		else if (op == "<=") {
+			ans = a <= b ? true : false;
+			print(ans, n);
+			n++;
+		}
+		else if (op == "==") {
+			ans = a == b ? true : false;
+			print(ans, n);
+			n++;
+		}
+		else if (op == "!=") {
+			ans = a != b ? true : false;
+			print(ans, n);
+			n++;
+		}
+			
+	}
+}
