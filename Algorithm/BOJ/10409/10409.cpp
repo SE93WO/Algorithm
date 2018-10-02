5c71364aea20d61293441ce151b2e7fc22ad2ee2
SEWO <Se93Wo@gmail.com>
2018-10-02 17:01:30 +0900

    SEWO

---
+++ Algorithm/BOJ/10409/10409.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	int n, t, arr[50], sum = 0, cnt = 0;
+	cin >> n >> t;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	for (int i = 0; i < n; i++) {
+		if (sum + arr[i] > t) break;
+		sum += arr[i];
+		cnt++;
+	}
+
+	cout << cnt;
+}
