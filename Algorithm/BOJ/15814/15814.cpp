2ec7c2e8995bcb1790fbf328d26ccef489bffb1f
SEWO <Se93Wo@gmail.com>
2018-10-06 00:20:41 +0900

    SEWO

---
+++ Algorithm/BOJ/15814/15814.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+	string S;
+	int T, A, B;
+
+	cin >> S;
+	cin >> T;
+	while (T--) {
+		cin >> A >> B;
+		char temp;
+
+		temp = S[A];
+		S[A] = S[B];
+		S[B] = temp;
+	}
+
+	cout << S;
+}
