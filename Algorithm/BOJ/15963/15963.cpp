99a82904da479d579f3f270508688490e0f76c5a
SEWO <Se93Wo@gmail.com>
2018-10-07 00:29:06 +0900

    SEWO

---
+++ Algorithm/BOJ/15963/15963.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+	ll N, M;
+
+	cin >> N >> M;
+	cout << (N == M ? "1" : "0");
+}
