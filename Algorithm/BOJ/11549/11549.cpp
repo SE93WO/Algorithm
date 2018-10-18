0edc359f9f95261bbda98ed175b53ad53b216452
SEWO <se93wo@gmail.com>
2018-10-18 15:30:59 +0900

    SEWO

---
+++ Algorithm/BOJ/11549/11549.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	int T, input, ans = 0;
+	
+	cin >> T;
+	for (int i = 0; i < 5; i++) {
+		cin >> input;
+		if (T == input)
+			ans++;
+	}
+	cout << ans;
+}
