fe2274ce1585feaf7f5d7d9db8044d8fe36b1a9b
SEWO <se93wo@gmail.com>
2018-12-12 01:20:54 +0900

    SEWO

---
+++ Algorithm/BOJ/16430/16430.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+
+	cout << B - A << " " << B;
+}
