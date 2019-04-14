c92b936689ae1ef88720e05d54e7b2455a929345
SEWO <se93wo@gmail.com>
2019-04-15 04:25:49 +0900

    농구 경기

---
+++ BOJ/1000/1159.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, count = 0;
+	int family_name[26] = { 0, };
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		char name[31];
+		cin >> name;
+		family_name[(int)(name[0] - 97)]++;
+	}
+
+	for (int i = 0; i < 26; i++)
+		if (family_name[i] >= 5) {
+			count++;
+			cout << (char)(i + 97);
+		}
+	
+	if (!count)
+		cout << "PREDAJA";
+}
