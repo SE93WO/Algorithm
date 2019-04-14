7cb25347a19c60b14e1927b03dc773aecba6ce74
SEWO <se93wo@gmail.com>
2019-04-15 04:40:53 +0900

    가장 큰 금민수

---
+++ BOJ/1000/1526.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, num;
+
+void ans(int x) {
+	if (x > n) return;
+	num = num < x ? x : num;
+	ans(x * 10 + 4);
+	ans(x * 10 + 7);
+}
+
+int main() {
+	cin >> n;
+	ans(0);
+	cout << num;
+}
