d074c15fd27e399399241482205778685a597994
SEWO <se93wo@gmail.com>
2019-04-15 04:26:10 +0900

    이상한 곱셈

---
+++ BOJ/1000/1225.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	long long x = 0, y = 0;
+	string A, B;
+
+	cin >> A >> B;
+
+	for (int i = 0; i < A.size(); i++)
+		x += A[i] - '0';
+	for (int i = 0; i < B.size(); i++)
+		y += x * (B[i] - '0');
+
+	cout << y;
+}
