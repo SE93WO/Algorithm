75a68da794e18769aca1b74dc452989c50207e5b
SEWO <se93wo@gmail.com>
2019-04-15 05:47:51 +0900

    최소공배수

---
+++ BOJ/1000/1934.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int EU(int x, int y) {
+	int n;
+	while (true) {
+		n = x % y;
+		if (n == 0) return y;
+		x = y;
+		y = n;
+	}
+}
+
+int main() {
+	int T;
+	cin >> T;
+
+	while (T--) {
+		int gcm, lcm;
+		int A, B;
+		cin >> A >> B;
+
+		gcm = EU(A, B);
+		lcm = A * B / gcm;
+
+		cout << lcm << endl;
+	}
+}
