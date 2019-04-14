d6f69fa3fad7cf44df28e999faccb3ceb146a5aa
SEWO <se93wo@gmail.com>
2019-04-15 04:14:07 +0900

    no message

--- BOJ/10699/10699.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << "2018-08-04";
-}
-
-
--- BOJ/1550/1550.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-int main() { 
-	int input;
-	cin >> hex >> input;
-	cout << input;
-}
