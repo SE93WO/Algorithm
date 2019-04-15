ed60fcb817e0cbcb5dbc337f352a91d645914cbc
SEWO <se93wo@gmail.com>
2019-04-15 16:19:22 +0900

    복붙의 달인

---
+++ BOJ/11000/11008.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	int T;
+	string s, p;
+
+	cin >> T;
+	while (T--) {
+		cin >> s >> p;
+		int time = 0;
+		while (s.find(p) != string::npos) {
+			s.erase(s.find(p), p.length());
+			time++;
+		}
+
+		time += s.length();
+
+		cout << time << endl;
+	}
+}
