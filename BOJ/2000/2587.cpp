f0885214299c058bc1e0e34167140ec0992c66ba
SEWO <se93wo@gmail.com>
2019-04-16 04:42:07 +0900

    대표값2

---
+++ BOJ/2000/2587.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int input[5];
+	int avg = 0, median = 0;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> input[i];
+	}
+
+	sort(input, input + 5);
+
+	for (int i = 0; i < 5; i++) {
+		avg += input[i];
+	}
+
+	cout << avg / 5 << "\n";
+	cout << input[2];
+ }
