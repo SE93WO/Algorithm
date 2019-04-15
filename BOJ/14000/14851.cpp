9a44e21fec5d0b64467e5219d7e8e7f0e1c47c23
SEWO <se93wo@gmail.com>
2019-04-15 19:18:40 +0900

    해밍 거리와 쿼리

---
+++ BOJ/14000/14851.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	cout << ":fan::fan::fan:" << endl;
+	cout << ":fan::" << str << "::fan:" << endl;
+	cout << ":fan::fan::fan:";
+}
