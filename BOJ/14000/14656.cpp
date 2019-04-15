371c9b16e3581c816a2c1fe2f2201c61c39fdd00
SEWO <se93wo@gmail.com>
2019-04-15 19:18:17 +0900

    조교는 새디스트야!!

---
+++ BOJ/14000/14656.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+	int N, input, cnt = 0;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> input;
+		if (input != i) cnt++;
+	}
+
+	cout << cnt;
+}
