293d298f7921f4a7323edc4d0594b8c3ac848849
SEWO <se93wo@gmail.com>
2019-04-16 04:42:39 +0900

    초6 수학

---
+++ BOJ/2000/2702.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int EU(int x, int y) {
+	int n;
+	while (true) {
+		n = x % y;
+		if (n == 0)
+			return y;
+		x = y;
+		y = n;
+	}
+}
+
+int main() {
+	int T, gcm, lcm;
+	int a, b;
+	cin >> T;
+	
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+		gcm = EU(a, b);
+		lcm = a * b / gcm;
+
+		cout << lcm << " " << gcm << endl;
+	}
+}
