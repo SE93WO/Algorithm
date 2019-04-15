100447b6aec8456645661dc7bd0facb13f0b7c97
SEWO <se93wo@gmail.com>
2019-04-16 04:44:02 +0900

    달팽이는 올라가고 싶다

---
+++ BOJ/2000/2869.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef long long lld;
+int main() {
+	lld a, b, v;
+	scanf("%lld %lld %lld", &a, &b, &v);
+
+	lld start, end, day;
+
+	start = 0;
+	end = 1000111000;
+	lld result = 1000111000;
+
+	while (start <= end) {
+		day = (start + end) / 2;
+
+		if (v <= day * (a - b) + a) {
+			result = (result > day + 1) ? day + 1 : result;
+			end = day - 1;
+		}
+		else if (v > day*(a - b) + a) {
+			start = day + 1;
+		}
+	}
+	printf("%lld", result);
+	return 0;
+}
