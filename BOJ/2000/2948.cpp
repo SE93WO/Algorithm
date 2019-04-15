285bdaea7a8434565276153777e93f6e81afe739
SEWO <se93wo@gmail.com>
2019-04-16 04:44:39 +0900

    2009년

---
+++ BOJ/2000/2948.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int Months[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	string days[] = {"Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"};
+	int D, M, ans = 0;
+
+	cin >> D >> M;
+
+	for (int i = 0; i < M - 1; i++) {
+		ans += Months[i];
+	}
+
+	ans += D;
+
+	cout << days[ans % 7];
+}
