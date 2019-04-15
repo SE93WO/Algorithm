07d7661aee214d0f843b4d3af7bc9222a362484f
SEWO <se93wo@gmail.com>
2019-04-15 15:45:02 +0900

    문자열 분석

---
+++ BOJ/10000/10820.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+	string N;
+
+	while (getline(cin, N), N != "") {
+		int lower = 0, upper = 0, digit = 0, space = 0;
+		
+		for (int i = 0; i < N.size(); i++) {
+			if (islower(N[i])) lower++;
+			else if (isupper(N[i])) upper++;
+			else if (isdigit(N[i])) digit++;
+			else if (isspace(N[i])) space++;
+		}
+
+		cout << lower << " " << upper << " " << digit << " " << space << endl;
+	}
+}
