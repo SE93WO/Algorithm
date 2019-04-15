962e878063043ea314a2c5fca83d54a17685a1f4
SEWO <se93wo@gmail.com>
2019-04-15 19:21:53 +0900

    홍익대학교

---
+++ BOJ/16000/16394.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	cout << N - 1946;
+}
