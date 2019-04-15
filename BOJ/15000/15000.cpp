84eeffc6eee9c4cf65d7cee970fe14380764cbd9
SEWO <se93wo@gmail.com>
2019-04-15 19:19:36 +0900

    CAPS

---
+++ BOJ/15000/15000.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	string input;
+	cin >> input;
+
+	for (int i = 0; i < input.size(); i++) {
+		input[i] = toupper(input[i]);
+	}
+	cout << input;
+}
