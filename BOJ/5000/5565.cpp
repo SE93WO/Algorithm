57983fb3d13194a431eed5f93d719b8aea2a9f9a
SEWO <se93wo@gmail.com>
2019-04-16 05:42:52 +0900

    영수증

---
+++ BOJ/5000/5565.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+	int sum, input;
+
+	cin >> sum;
+	for (int i = 0; i < 9; i++)
+		cin >> input, sum -= input;
+
+	cout << sum;
+}
