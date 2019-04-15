17460f4eedb5216298902764cdd4ecf57023ff70
SEWO <se93wo@gmail.com>
2019-04-16 04:38:46 +0900

    이친수

---
+++ BOJ/2000/2193.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main() {
+	int N;
+	long long dp[91] = { 0, 1, 1 };
+	cin >> N;
+
+	for (int i = 3; i <= N; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	
+	cout << dp[N];
+}
