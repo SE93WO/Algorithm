229e7dc26400e0aacc9b38ea68deba57ec3488ea
SEWO <se93wo@gmail.com>
2019-04-16 05:40:32 +0900

    Quicksum

---
+++ BOJ/4000/4613.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string input;
+
+	while (true) {
+		int sum = 0;
+		getline(cin, input);
+		if (input == "#") break;
+
+		for (int i = 0; i < input.length(); i++) {
+			if (isalpha(input[i])) {
+				sum += (i + 1) * (int)(input[i] - 'A' + 1);
+			}
+		}
+		cout << sum << endl;
+	}
+}
