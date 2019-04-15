9c9a8ed6b0478b83d89db4c40c8762117f3dc78a
SEWO <se93wo@gmail.com>
2019-04-15 15:44:38 +0900

    공 바꾸기

---
+++ BOJ/10000/10813.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N, M;
+	int bucket[101];
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++)
+		bucket[i] = i;
+
+	for (int i = 0; i < M; i++) {
+		int x, y;
+		cin >> x >> y;
+		swap(bucket[x], bucket[y]);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		cout << bucket[i] << " ";
+	}
+}
