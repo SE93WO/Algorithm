ffddcb742c800e1f647aae401c769bd0f7a69d39
SEWO <se93wo@gmail.com>
2019-04-16 05:42:40 +0900

    유니크

---
+++ BOJ/5000/5533.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N, input;
+	int player[201][3] = { 0, };
+	int ans[201] = { 0, };
+	int score[101][3] = { 0, };
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> input;
+			score[input][j]++;
+			player[i][j] = input;
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < 3; j++) {
+				if (score[player[i][j]][j] == 1)
+					ans[i] += player[i][j];
+		}
+	}
+	
+	for (int i = 0; i < N; i++) {
+		cout << ans[i] << endl;
+	}
+}
