653969c4682f4e1035e1c2c284c163793c348c79
SEWO <se93wo@gmail.com>
2019-04-16 04:40:38 +0900

    검증수

---
+++ BOJ/2000/2475.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int input[5];
+	int ans = 0;
+
+	for (int i = 0; i < 5; i++)
+		cin >> input[i];
+	
+	for (int i = 0; i < 5; i++) {
+		ans += input[i] * input[i];
+	}
+
+	cout << ans % 10;
+}
