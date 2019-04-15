74fdfa162491bad534fd6a9068a7e678a22b28a4
SEWO <se93wo@gmail.com>
2019-04-16 04:38:30 +0900

    소수 단어

---
+++ BOJ/2000/2153.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int temp, sum = 0;
+	string input;
+	
+	cin >> input;
+
+	for (int i = 0; i < input.length(); i++) {
+		temp = input[i] - '0';
+		if (temp >= 49 && temp <= 74)
+			temp -= 48;
+		else if (temp >= 17 && temp <= 42)
+			temp += 10;
+		sum += temp;
+	}
+
+	for (temp  = 2; temp < sum; temp++) {
+		if (sum % temp == 0) {
+			cout << "It is not a prime word.";
+			break;
+		}
+	}
+
+	if (temp == sum || sum == 1)
+		cout << "It is a prime word.";
+
+}
