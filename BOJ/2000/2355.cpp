6aecc9355e0153538a2bdee7b6daadeaacf3d3a2
SEWO <se93wo@gmail.com>
2019-04-16 04:39:21 +0900

    시그마

---
+++ BOJ/2000/2355.cpp
@@ -0,0 +1,13 @@
+﻿#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	long long A, B;
+	scanf("%lld %lld", &A, &B);
+	if (A > B)
+		swap(A, B);
+
+	printf("%lld\n", (B - A + 1) * (A + B) / 2);
+	
+}
