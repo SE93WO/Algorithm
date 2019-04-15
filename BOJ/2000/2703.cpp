4a4fe6ea2beaae012c5e69213725576af6e5528f
SEWO <se93wo@gmail.com>
2019-04-16 04:42:44 +0900

    Cryptoquote

---
+++ BOJ/2000/2703.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string word, alphabat;
+	int T;
+
+	cin >> T;
+	while (T--) {
+		cin.ignore(1000, '\n');
+		getline(cin, word);
+		cin >> alphabat;
+
+		for (char &c : word) {
+			if (c != ' ')
+				c = alphabat[c - 'A'];
+		}
+
+		cout << word << endl;
+	}
+
+ }
