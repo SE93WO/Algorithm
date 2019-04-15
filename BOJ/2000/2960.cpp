e632c2af6cc7a66da9a0500522ae1b35e7a0c68f
SEWO <se93wo@gmail.com>
2019-04-16 04:44:59 +0900

    에라토스테네스의 체

---
+++ BOJ/2000/2960.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N, K;
+	int cnt = 0;
+	bool num[1001] = { 0, };
+
+	cin >> N >> K;
+
+	for (int i = 2; i <= N; i++) {
+		if (num[i])
+			continue;
+		for (int j = i; j <= N; j += i) {
+			if (num[j])
+				continue;
+			cnt++;
+			num[j] = true;
+			if (cnt == K) {
+				cout << j << endl;
+				break;
+			}
+		}
+	}
+}
