0c204ffe23c82c050dcb4d41a54b121f7a7f7419
SEWO <se93wo@gmail.com>
2019-04-16 04:42:49 +0900

    오타맨 고창영

---
+++ BOJ/2000/2711.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int T;
+
+	cin >> T;
+	while (T--) {
+		int pos;
+
+		string input;
+		
+		cin >> pos;
+		cin >> input;
+
+		input.erase(pos - 1, 1);
+
+		cout << input << "\n";
+	}
+
+ }
