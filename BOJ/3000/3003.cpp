63b8b8fb7bba7a02dff7a211979694b9abcdb291
SEWO <se93wo@gmail.com>
2019-05-26 09:29:57 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ BOJ/3000/3003.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int arr[6] = { 1, 1, 2, 2, 2, 8 };
+	for (int i = 0; i < 6; i++) {
+		int input;
+		cin >> input;
+		cout << arr[i] - input << " ";
+	}
+}
