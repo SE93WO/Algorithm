4948ef5e9b50489aeb07740f654f5f835745f457
SE93WO <se93wo@gmail.com>
2019-09-21 19:01:11 +0900

    개미

---
+++ BOJ/10000/10158.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int w, h, x, y, t;
+int main() {
+	scanf("%d %d %d %d %d", &w, &h, &x, &y, &t);
+	printf("%d %d", w - abs(w - (x + t) % (2 * w)), h - abs(h - (y + t) % (2 * h)));
+	return 0;
+}
