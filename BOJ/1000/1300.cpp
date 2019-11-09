596a4dfa10960c787140a51b2a047f9e70e6efbb
SE93WO <se93wo@gmail.com>
2019-11-10 08:41:42 +0900

    K번째 수

---
+++ BOJ/1000/1300.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+	int left = 1, right = k, ans = 0;
+	while (left <= right) {
+		long long cnt = 0;
+		int mid = (left + right) / 2;
+		for (int i = 1; i <= n; i++) {
+			cnt += min(mid / i, n);
+		}
+		if (cnt < k) {
+			left = mid + 1;
+		}
+		else {
+			ans = mid;
+			right = mid - 1;
+		}
+	}
+	cout << ans;
+}
